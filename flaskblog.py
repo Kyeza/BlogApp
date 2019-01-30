3ba9a2515abfd00fb9709b52f9830a7eafa22c89
kyeza <kyezaarnold63@gmail.com>
2019-01-30 11:34:11 +0300

    new file:   templates/layout.html - Added templates for a base layout, home and about - Added bootstrap and custom css

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
 """Blog flask app"""
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 APP = Flask("__name__")
+
+# dummy data
+posts = [
+	{
+		'author': 'Corey Schafer',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'April 20, 2018'
+	},
+	{
+		'author': 'Kyeza Arnold',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'April 21, 2018'
+	}
+]
 
 @APP.route("/")
 @APP.route("/home")
 def home():
 	"""Home view"""
-	return "<h1>Home Page</h1>"
+	return render_template('home.html', posts=posts)
 
 @APP.route("/about")
 def about():
 	"""About view"""
-	return "<h1>About Page</h1>"
-	
+	return render_template('about.html', posts=posts)
