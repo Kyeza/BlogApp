888ac5abc75aff75d32a18222ca3ed2964290e7c
kyeza <kyezaarnold63@gmail.com>
2019-02-01 21:14:53 +0300

    - Install flask-bcrypt - Installed flask-login - Implemented login, logout and registration system - Added account template

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -11,6 +12,16 @@ class RegistrationForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password', 
 		validators=[DataRequired(), Length(min=8), EqualTo('password')])
 	submit = SubmitField('Sign Up')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError(f'User already exists with {username.data}')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError(f'User already exists with {email.data}')
 
 	
 class LoginForm(FlaskForm):
---
+++ run.py
@@ -0,0 +1,6 @@
+"""module for running app"""
+
+from flaskblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
