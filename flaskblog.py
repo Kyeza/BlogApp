ec5d0b1f26747f9f57ebb87f8b0e368b6ba00d22
kyeza <kyezaarnold63@gmail.com>
2019-02-01 13:16:21 +0300

    - Designed login system for the blog app - Installed form-wtf for creating flask forms - Added login and registration templates and routes - Added secret CSRF key using secrets.token_hex(16)

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,10 @@
 """Blog flask app"""
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 APP = Flask("__name__")
+APP.config['SECRET_KEY'] = '610c7d4cf574d1106403c21937017696'
 
 # dummy data
 posts = [
@@ -29,4 +31,29 @@ def home():
 @APP.route("/about")
 def about():
 	"""About view"""
-	return render_template('about.html', posts=posts)
+	return render_template('about.html', title='About')
+
+@APP.route("/register", methods=['GET', 'POST'])
+def register():
+	"""Register view"""
+	form = RegistrationForm()
+
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+
+	return render_template('register.html', title='Register', form=form)
+
+@APP.route("/login", methods=['GET', 'POST'])
+def login():
+	"""Login view"""
+	form = LoginForm()
+	
+	if form.validate_on_submit():
+		if form.email.data == 'k@gmail.com' and form.password.data == '12345678':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')
+
+	return render_template('login.html', title='Login', form=form)
