2c9d3ac9623018122187bc26efbb56ff1ce79a50
kyeza <kyezaarnold63@gmail.com>
2019-02-02 15:20:28 +0300

    Implemented Profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flaskblog.models import User
@@ -30,4 +32,24 @@ class LoginForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+	"""docstring for UpdateAccountForm"""
+	username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+	submit = SubmitField('Update')
+
+	def validate_username(self, username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError(f'User already exists with {username.data}')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError(f'User already exists with {email.data}')
 		
