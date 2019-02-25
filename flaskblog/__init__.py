c4dafdbeb221d4a8aaba647ceebece344ebb028f
kyeza <kyezaarnold63@gmail.com>
2019-02-25 10:15:12 +0300

    Finished all main functionality of the app

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,18 +1,40 @@
 """Blog flask app"""
 
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
 
-app = Flask("__name__")
-app.config['SECRET_KEY'] = '610c7d4cf574d1106403c21937017696'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-from flaskblog import routes
+mail = Mail()
+
+
+def create_app(config_class=Config):
+	app = Flask("__name__")
+	app.config.from_object(Config)
+
+	db.init_app(app)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
+
+	from flaskblog.users.routes import users
+	from flaskblog.posts.routes import posts
+	from flaskblog.main.routes import main
+	from flaskblog.errors.handlers import errors
+
+	app.register_blueprint(users)
+	app.register_blueprint(posts)
+	app.register_blueprint(main)
+	app.register_blueprint(errors)
+
+	return app
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField,  PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flaskblog.models import User
 
 
@@ -52,11 +52,22 @@ class UpdateAccountForm(FlaskForm):
 			user = User.query.filter_by(email=email.data).first()
 			if user:
 				raise ValidationError(f'User already exists with {email.data}')
-		
 
-class PostForm(FlaskForm):
-	"""docstring for PostForm"""
-	title = StringField('Title', validators=[DataRequired()])
-	content = TextAreaField('Content', validators=[DataRequired()])
-	submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+	"""docstring for RequestResetForm"""
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	submit = SubmitField('Request Password Reset')
 		
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is None:
+			raise ValidationError(f'There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+	"""docstring for ResetPasswordForm"""
+	password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+	confirm_password = PasswordField('Confirm Password', 
+		validators=[DataRequired(), Length(min=8), EqualTo('password')])
+	submit = SubmitField('Reset Password')
